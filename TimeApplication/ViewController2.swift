0e8c279a807a24544a34087c8d0ca2d35861cc07
kazuki <firstshining.flowerflower.yui@gmail.com>
2018-01-02 15:15:10 +0900

    add pickerview

--- TimeApplication/ViewController2.swift
+++ TimeApplication/ViewController2.swift
@@ -8,12 +8,36 @@
 
 import UIKit
 
-class ViewController2: UIViewController {
+class ViewController2: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
 
+    
+    @IBOutlet weak var TimerView: UIPickerView!
+    let timeList = [[Int](0...24),[Int](0...59),[Int](0...59)]
+    @IBOutlet weak var timeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return timeList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return timeList[component].count
+    }
+    
+    private func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> Int? {
+        return timeList[component][row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let hour = self.pickerView(pickerView, titleForRow: 0, forComponent: 0)
+        let minute = self.pickerView(pickerView, titleForRow: 1, forComponent: 1)
+        let second = self.pickerView(pickerView, titleForRow: 2, forComponent: 2)
+        timeLabel.text = hour?.description
+        
     }
 
     override func didReceiveMemoryWarning() {
